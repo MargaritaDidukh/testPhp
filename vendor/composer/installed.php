0a5335b48be5fcfc27f10989ced482f7b7422635
MargaritaDidukh <margartadidukh16@gmail.com>
2022-04-18 20:02:50 +0200

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'margo/test-php',
+  ),
+  'versions' => 
+  array (
+    'margo/test-php' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'squizlabs/php_codesniffer' => 
+    array (
+      'pretty_version' => '3.6.2',
+      'version' => '3.6.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5e4e71592f69da17871dba6e80dd51bce74a351a',
+    ),
+  ),
+);
