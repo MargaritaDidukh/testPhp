2fdf9c505d944e48010773c7ed6198f0ff92ac70
MargaritaDidukh <margartadidukh16@gmail.com>
2022-04-19 16:10:50 +0200

    fix to fifth task

--- taskFifth.php
+++ taskFifth.php
@@ -3,5 +3,5 @@ $handle = fopen('php://stdin', 'r');
 echo 'Enter x' . "\n";
 $x = fgets($handle);
 
-echo('$x%3 = ' . $x % 3 . "\n" . '$x%5 = ' . $x % 5 . "\n");
+echo('$x % 3 = ' . $x % 3 . "\n" . '$x % 5 = ' . $x % 5 . "\n");
 echo('$x * 30% = ' . $x + ($x * 0.3) . "\n" . '$x * 120% = ' . $x + ($x * 1.2) . "\n");
